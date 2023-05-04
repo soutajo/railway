03d2345fd14f80d8ee28493c3e8c246e082e7397
soutajo <roninghost42@gmail.com>
2023-05-04 18:31:59 +0300

    Added 'Welcome' page. Added functional to create, show, edit and delete railway stations.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :railway_stations
+  get 'welcome/index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'welcome#index'
+  
 end
